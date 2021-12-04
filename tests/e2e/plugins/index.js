ed001c87ac81af6fed7156cc85638ea05cfaa487
Killian Hascoet <killianh@live.fr>
2021-12-04 19:43:08 +0100

    Suppress warning

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,4 +1,5 @@
 module.exports = (on, config) => {
+  // eslint-disable-next-line global-require
   require('@cypress/code-coverage/task')(on, config);
   return { ...config,
     fixturesFolder: 'tests/e2e/fixtures',
