7fed395cdabd8b66b60b4f1c4a29c8031e737f3d
Killian Hascoet <killianh@live.fr>
2022-01-05 23:50:55 +0100

    Fix service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,23 +3,12 @@ if (workbox) {
   console.log('Workbox is loaded');
   // eslint-disable-next-line no-restricted-globals
   self.skipWaiting();
+
   // eslint-disable-next-line no-undef,no-restricted-globals,no-underscore-dangle
   workbox.precaching.precacheAndRoute(self.__precacheManifest);
-  // eslint-disable-next-line no-undef
-  workbox.precaching.precacheAndRoute([
-    { url: '/', revision: null },
-    { url: '/settings', revision: null },
-    { url: '/balance', revision: null },
-    { url: '/transfer', revision: null },
-    { url: '/account', revision: null },
-    { url: '/stake', revision: null },
-    { url: '/unstake', revision: null },
-    { url: '/addbid', revision: null },
-    { url: '/withdrawbid', revision: null },
-    { url: '/smartcontract', revision: null },
-    { url: '/faq', revision: null },
-    { url: '/contact', revision: null },
-  ]);
+
+  // eslint-disable-next-line no-undef,no-restricted-globals,no-underscore-dangle
+  workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('index.html'));
   // eslint-disable-next-line no-undef
   workbox.routing.registerRoute(
     new RegExp(/\.(?:jpg|webp|mp4|woff2|jpeg|png|gif|ico|css|html|md)$/),
@@ -27,7 +16,7 @@ if (workbox) {
     new workbox.strategies.StaleWhileRevalidate(),
   );
   // eslint-disable-next-line no-undef
-  workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate());
+  workbox.routing.setDefaultHandler(new workbox.strategies.NetworkFirst());
 } else {
   console.log('Workbox didn\'t load');
 }
