050cae93eba43fd0ce403b77dd3294fb453bdc38
Killian Hascoet <killianh@live.fr>
2022-03-17 01:13:02 +0100

    Result component improvement

--- src/components/operations/OperationPendingWeight.vue
+++ src/components/operations/OperationPendingWeight.vue
@@ -1,111 +1,103 @@
 <template>
-  <v-card
-    class="align-center rounded-xl secondary mt-5 operationResult"
-    width="100%"
-  >
-    <v-card-title>
-      Deploys missing weight
-    </v-card-title>
-    <v-card-subtitle>
-      Some deploys needs more weight before being sent.
-    </v-card-subtitle>
-    <v-card-text
-      class="text-body-1"
+  <div>
+    <v-card
+      v-for="(weightedDeploy,i) in weightedDeploys"
+      :key="i"
+      class="align-center rounded-xl secondary mt-5 operationResult"
+      width="100%"
     >
-      <v-expansion-panels accordion>
-        <v-expansion-panel
-          v-for="(weightedDeploy,i) in weightedDeploys"
-          :key="i"
-        >
-          <v-expansion-panel-header v-if="!weightedDeploy.error">
-            {{ weightedDeploy.deployResult.name }} - {{
-              truncate(weightedDeploy.deployResult.hash)
-            }}
-          </v-expansion-panel-header>
-          <v-expansion-panel-header
-            v-if="weightedDeploy.error"
-            disable-icon-rotate
+      <v-card-title>
+        {{ weightedDeploy.deployResult.name }} - Deploy missing weight
+      </v-card-title>
+      <v-card-subtitle>
+        {{ weightedDeploy.deployResult.hash }}
+      </v-card-subtitle>
+      <v-card-text
+        class="text-body-1"
+      >
+        <p>
+          The deploy needs to be signed by others parties.
+        </p>
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+            class="py-0"
           >
-            {{ weightedDeploy.deployResult.name }} - {{
-              truncate(weightedDeploy.deployResult.hash)
-            }}
-            <template #actions>
-              <v-icon color="error">
-                mdi-alert-circle
-              </v-icon>
-            </template>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <div class="mx-n1 text-body-1 text-center mb-6">
-              <p v-if="weightedDeploy.error">
-                <v-icon
-                  color="red"
-                  left
-                >
-                  mdi-alert-circle
-                </v-icon>
-                Oops... A problem as occurred. <br>
-                Check the error message here (or on the cspr.live website) : <br>
-                <strong> {{ weightedDeploy.error.toString() }} </strong> <br>
-              </p>
-              <v-btn
-                class="rounded-xl mt-3 mr-3"
-                color="primary"
-                @click="createLink(i)"
-              >
-                <v-icon
-                  color="white"
-                  left
-                >
-                  mdi-content-copy
-                </v-icon>
-                Create sharable link
-              </v-btn>
-              <v-btn
-                class="rounded-xl mt-3 mr-3"
-                color="primary"
-                @click="copyDeploy(i)"
-              >
-                <v-icon
-                  color="white"
-                  left
-                >
-                  mdi-content-copy
-                </v-icon>
-                Copy JSON Deploy
-              </v-btn>
-              <v-btn
-                class="rounded-xl mt-3"
-                color="red"
-                @click="cancelSendingDeploy(i)"
-              >
-                <v-icon
-                  color="white"
-                  left
-                >
-                  mdi-cancel
-                </v-icon>
-                Cancel
-              </v-btn>
-            </div>
-            <div
-              v-if="urls[weightedDeploy.deployResult.hash]"
-              class="mx-n1 text-body-1 text-center mb-6"
+            <v-btn
+              class="rounded-xl mt-3 mr-3"
+              style="width: 100%"
+              color="primary"
+              @click="copyLink(i)"
             >
-              Url : <a :href="urls[weightedDeploy.deployResult.hash]">link</a>
-            </div>
-            Details :
-            <vue-json-pretty
-              class="overflow-auto"
-              style="height: 500px; background-color: black;"
-              :data="deployJson(weightedDeploy.deploy)"
-              :show-line="true"
-            />
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-card-text>
-  </v-card>
+              <v-icon
+                color="white"
+                left
+              >
+                mdi-link
+              </v-icon>
+              Copy link
+            </v-btn>
+          </v-col>
+          <v-col
+            cols="12"
+            md="4"
+            class="py-0"
+          >
+            <v-btn
+              class="rounded-xl mt-3 mr-3"
+              style="width: 100%"
+              color="primary"
+              @click="copyDeploy(i)"
+            >
+              <v-icon
+                color="white"
+                left
+              >
+                mdi-content-copy
+              </v-icon>
+              Copy JSON Deploy
+            </v-btn>
+          </v-col>
+          <v-col
+            cols="12"
+            md="4"
+            class="pt-0"
+          >
+            <v-btn
+              class="rounded-xl mt-3"
+              style="width: 100%"
+              color="red"
+              @click="cancelSendingDeploy(i)"
+            >
+              <v-icon
+                color="white"
+                left
+              >
+                mdi-cancel
+              </v-icon>
+              Cancel
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-expansion-panels accordion>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              Details
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <vue-json-pretty
+                class="overflow-auto"
+                style="height: 500px; background-color: black;"
+                :data="deployJson(weightedDeploy.deploy)"
+                :show-line="true"
+              />
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -156,7 +148,7 @@ export default {
         JSON.stringify(this.deployJson(this.weightedDeploys[index].deploy)),
       );
     },
-    async createLink(index) {
+    async copyLink(index) {
       const res = await fetch(`${API}/deploys/`, {
         method: 'POST',
         headers: {
@@ -171,10 +163,18 @@ export default {
         const text = await res.text();
         if (text.includes('Deploy already exist')) {
           this.$set(this.urls, this.deployJson(this.weightedDeploys[index].deploy).deploy.hash, `/multisig/${this.deployJson(this.weightedDeploys[index].deploy).deploy.hash}`);
+          await navigator.clipboard.writeText(
+            `${window.location.origin}/multisig/${this.deployJson(this.weightedDeploys[index].deploy).deploy.hash}`,
+          );
+          return;
         }
         this.$set(this.urls, this.deployJson(this.weightedDeploys[index].deploy).deploy.hash, 'Unknown error');
+        return;
       }
       this.$set(this.urls, this.deployJson(this.weightedDeploys[index].deploy).deploy.hash, `/multisig/${this.deployJson(this.weightedDeploys[index].deploy).deploy.hash}`);
+      await navigator.clipboard.writeText(
+        `${window.location.origin}/multisig/${this.deployJson(this.weightedDeploys[index].deploy).deploy.hash}`,
+      );
     },
     cancelSendingDeploy(index) {
       this.$store.dispatch('removeWeightDeploy', index);
--- src/views/multisig/MultiSig.vue
+++ src/views/multisig/MultiSig.vue
@@ -1,219 +1,247 @@
 <template>
-  <v-card
-    class="align-center rounded-xl secondary"
-    width="100%"
-  >
-    <v-card-title class="align-center">
-      <v-avatar
-        color="primary"
-        size="52"
-      >
-        <v-icon>mdi-key-chain</v-icon>
-      </v-avatar>
-      <v-card-title
-        class="pl-4"
-        style="word-break: break-word"
-      >
-        Sign a multisig deploy
-      </v-card-title>
-    </v-card-title>
-    <v-card-text>
-      <v-card
-        outlined
-        elevation="3"
-        class="mb-4"
-      >
-        <v-card-title>
-          <v-icon left>
-            mdi-clipboard-list
-          </v-icon>
-          Summary
+  <div>
+    <v-card
+      class="align-center rounded-xl secondary"
+      width="100%"
+    >
+      <v-card-title class="align-center">
+        <v-avatar
+          color="primary"
+          size="52"
+        >
+          <v-icon>mdi-key-chain</v-icon>
+        </v-avatar>
+        <v-card-title
+          class="pl-4"
+          style="word-break: break-word"
+        >
+          Sign a multisig deploy
         </v-card-title>
-
-        <v-card-text>
-          <div class="mx-n1">
-            <v-row
-              class="white-bottom-border"
+      </v-card-title>
+      <v-card-text>
+        <v-card
+          outlined
+          elevation="3"
+          class="mb-4"
+        >
+          <v-card-title>
+            <v-icon left>
+              mdi-clipboard-list
+            </v-icon>
+            Summary
+          </v-card-title>
+          <v-card-text>
+            <div class="mx-n1">
+              <v-row
+                class="white-bottom-border"
+              >
+                <v-col>Account</v-col>
+                <v-col class="text-right cspr">
+                  {{ account }}
+                </v-col>
+              </v-row>
+              <v-row
+                class="white-bottom-border"
+              >
+                <v-col>Operation Name</v-col>
+                <v-col class="text-right cspr">
+                  {{ name }}
+                </v-col>
+              </v-row>
+              <v-row
+                class="white-bottom-border"
+              >
+                <v-col>Amount</v-col>
+                <v-col class="text-right cspr">
+                  {{ amount }} CSPR
+                </v-col>
+              </v-row>
+              <v-row
+                class="white-bottom-border"
+              >
+                <v-col>Fee</v-col>
+                <v-col class="text-right cspr">
+                  {{ cost }} CSPR
+                </v-col>
+              </v-row>
+              <v-row
+                class="white-bottom-border"
+              >
+                <v-col>Approvals</v-col>
+                <v-col class="text-right cspr">
+                  <template v-for="(approval, i) in approvals">
+                    <div :key="i">
+                      {{ approval.signer }}
+                    </div>
+                  </template>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-expansion-panels accordion>
+                  <v-expansion-panel>
+                    <v-expansion-panel-header>
+                      Details
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <vue-json-pretty
+                        class="overflow-auto"
+                        style="height: 500px; background-color: black;"
+                        :data="deployJson()"
+                        :show-line="true"
+                      />
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-expansion-panels>
+              </v-row>
+            </div>
+          </v-card-text>
+        </v-card>
+        <template v-if="countdown && !alreadySigned">
+          <p>
+            <template
+              v-if="validated"
             >
-              <v-col>Account</v-col>
-              <v-col class="text-right cspr">
-                {{ account }}
-              </v-col>
-            </v-row>
-            <v-row
-              class="white-bottom-border"
+              <v-icon
+                color="success"
+                left
+              >
+                mdi-checkbox-marked-circle
+              </v-icon>
+              Account authorized to sign this deploy.
+            </template>
+            <template v-else>
+              <v-icon
+                color="error"
+                left
+              >
+                mdi-alert-circle
+              </v-icon>
+              Account not authorized to sign this deploy.
+            </template>
+          </p>
+          <p>
+            <v-icon
+              color="info"
+              left
             >
-              <v-col>Operation Name</v-col>
-              <v-col class="text-right cspr">
-                {{ name }}
-              </v-col>
-            </v-row>
-            <v-row
-              class="white-bottom-border"
+              mdi-information
+            </v-icon>
+            Current deploy weight : {{ currentWeight }}
+          </p>
+          <p>
+            <v-icon
+              color="info"
+              left
             >
-              <v-col>Amount</v-col>
-              <v-col class="text-right cspr">
-                {{ amount }} CSPR
-              </v-col>
-            </v-row>
-            <v-row
-              class="white-bottom-border"
+              mdi-information
+            </v-icon>
+            Current key weight : {{ currentKeyWeight }}
+          </p>
+          <p>
+            <v-icon
+              color="info"
+              left
             >
-              <v-col>Fee</v-col>
-              <v-col class="text-right cspr">
-                {{ cost }} CSPR
-              </v-col>
-            </v-row>
-            <v-row
-              class="white-bottom-border"
-            >
-              <v-col>Approvals</v-col>
-              <v-col class="text-right cspr">
-                <template v-for="(approval, i) in approvals">
-                  <div :key="i">
-                    {{ approval.signer }}
-                  </div>
-                </template>
-              </v-col>
-            </v-row>
-          </div>
-        </v-card-text>
-      </v-card>
-      <template v-if="countdown && !alreadySigned">
-        <p>
-          <template
-            v-if="validated"
-          >
+              mdi-information
+            </v-icon>
+            Weight needed : {{ weightNeeded }}
+          </p>
+          <p>
             <v-icon
               color="success"
               left
             >
               mdi-checkbox-marked-circle
             </v-icon>
-            Account authorized to sign this deploy.
-          </template>
-          <template v-else>
+            This deploy is still valid, but it will expire in {{ countdown }}.
+          </p>
+          <p>
+            <template
+              v-if="(currentWeight + currentKeyWeight) >= weightNeeded"
+            >
+              <v-icon
+                color="success"
+                left
+              >
+                mdi-checkbox-marked-circle
+              </v-icon>
+              Deploy will be sent right after you sign it since it will reach the required weight.
+            </template>
+            <template v-else>
+              <v-icon
+                color="error"
+                left
+              >
+                mdi-alert-circle
+              </v-icon>
+              The deploy will still miss {{ weightNeeded - currentWeight - currentKeyWeight }}
+              weight before sending it.
+            </template>
+          </p>
+        </template>
+        <template v-else>
+          <p v-if="!countdown">
             <v-icon
               color="error"
               left
             >
               mdi-alert-circle
             </v-icon>
-            Account not authorized to sign this deploy.
-          </template>
-        </p>
-        <p>
-          <v-icon
-            color="info"
-            left
-          >
-            mdi-information
-          </v-icon>
-          Current deploy weight : {{ currentWeight }}
-        </p>
-        <p>
-          <v-icon
-            color="info"
-            left
-          >
-            mdi-information
-          </v-icon>
-          Current key weight : {{ currentKeyWeight }}
-        </p>
-        <p>
-          <v-icon
-            color="info"
-            left
-          >
-            mdi-information
-          </v-icon>
-          Weight needed : {{ weightNeeded }}
-        </p>
-        <p>
-          <v-icon
-            color="success"
-            left
-          >
-            mdi-checkbox-marked-circle
-          </v-icon>
-          This deploy is still valid, but it will expire in {{ countdown }}.
-        </p>
-        <p>
-          <template
-            v-if="(currentWeight + currentKeyWeight) >= weightNeeded"
-          >
+            Unfortunately this deploy is expired.
+          </p>
+          <p v-if="alreadySigned">
             <v-icon
               color="success"
               left
             >
               mdi-checkbox-marked-circle
             </v-icon>
-            Deploy will be sent right after you sign it since it will reach the required weight.
-          </template>
-          <template v-else>
-            <v-icon
-              color="error"
-              left
-            >
-              mdi-alert-circle
-            </v-icon>
-            The deploy will still miss {{ weightNeeded - currentWeight - currentKeyWeight }}
-            weight before sending it.
-          </template>
-        </p>
-      </template>
-      <template v-else>
-        <p v-if="!countdown">
-          <v-icon
-            color="error"
-            left
-          >
-            mdi-alert-circle
+            You already signed this deploy.
+          </p>
+        </template>
+        <v-alert
+          v-if="errorSign"
+          class="mt-5"
+          dense
+          prominent
+          type="error"
+        >
+          {{ errorSign.message }}
+        </v-alert>
+      </v-card-text>
+      <v-card-actions class="pa-4">
+        <v-btn
+          id="agreeAndSign"
+          class="rounded-xl"
+          color="primary"
+          dark
+          large
+          :disabled="!countdown || alreadySigned"
+          :loading="signingLoading"
+          @click="signMultisig"
+        >
+          <v-icon left>
+            mdi-draw
           </v-icon>
-          Unfortunately this deploy is expired.
-        </p>
-        <p v-if="alreadySigned">
-          <v-icon
-            color="error"
-            left
-          >
-            mdi-checkbox-marked-circle
-          </v-icon>
-          You already signed this deploy.
-        </p>
-      </template>
-      <v-alert
-        v-if="errorSign"
-        class="mt-5"
-        dense
-        prominent
-        type="error"
-      >
-        {{ errorSign.message }}
-      </v-alert>
-    </v-card-text>
-    <v-card-actions class="pa-4">
-      <v-btn
-        id="agreeAndSign"
-        class="rounded-xl"
-        color="primary"
-        dark
-        large
-        :disabled="!countdown && alreadySigned"
-        :loading="signingLoading"
-        @click="signMultisig"
-      >
-        <v-icon left>
-          mdi-draw
-        </v-icon>
-        Agree & Sign
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+          Agree & Sign
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <OperationResult
+      v-for="operation in filteredOperations"
+      :key="operation.hash"
+      :deploy-hash="operation.hash"
+    />
+    <OperationPending v-if="offlineDeploys.length > 0" />
+    <OperationPendingWeight v-if="weightedDeploys.length > 0" />
+  </div>
 </template>
 
 <script>
 
+import OperationPending from '@/components/operations/OperationPending';
+import OperationPendingWeight from '@/components/operations/OperationPendingWeight';
+import OperationResult from '@/components/operations/OperationResult';
 import clientCasper from '@/helpers/clientCasper';
 import deployManager from '@/helpers/deployManager';
 import { API } from '@/helpers/env';
@@ -224,12 +252,20 @@ import { CLPublicKey, DeployUtil } from 'casper-js-sdk';
 import parse from 'parse-duration';
 import countdown from 'countdown';
 import { mapGetters, mapState } from 'vuex';
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
 
 /**
  * Multi-sig View
  */
 export default {
   name: 'MultiSig',
+  components: {
+    VueJsonPretty,
+    OperationPendingWeight,
+    OperationPending,
+    OperationResult,
+  },
   data() {
     return {
       deploy: {},
@@ -247,12 +283,20 @@ export default {
   computed: {
     ...mapState([
       'signer',
+      'offlineDeploys',
+      'weightedDeploys',
+      'operations',
     ]),
     ...mapGetters([
       'signerObject',
       'signerOptionsFactory',
       'activeKey',
     ]),
+    filteredOperations() {
+      return this.operations.filter(
+        (operation) => operation.hash === this.$route.params.deployHash,
+      );
+    },
     account() {
       return this.deploy?.deploy ? this.deploy.deploy.deploy.header.account : '';
     },
@@ -288,6 +332,9 @@ export default {
     await this.getKeyInfos();
   },
   methods: {
+    deployJson() {
+      return this.deploy.deploy;
+    },
     async getKeyInfos() {
       if (this.signer.activeKey && this.keyInfo) {
         const account = this.keyInfo.Account.associatedKeys.find(
@@ -323,17 +370,19 @@ export default {
         });
         const ttl = parse(this.deploy.deploy.deploy.header.ttl);
         const timestamp = Date.parse(this.deploy.deploy.deploy.header.timestamp);
-        const countdownID = countdown((ts) => {
-          if (ts.value > 0) {
-            this.countdown = ts.toString();
-          } else {
-            this.countdown = null;
-            clearInterval(countdownID);
-          }
-        },
-        ttl + timestamp,
-        // eslint-disable-next-line no-bitwise
-        countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+        const countdownID = countdown(
+          (ts) => {
+            if (ts.value > 0) {
+              this.countdown = ts.toString();
+            } else {
+              this.countdown = null;
+              clearInterval(countdownID);
+            }
+          },
+          ttl + timestamp,
+          // eslint-disable-next-line no-bitwise
+          (countdown.HOURS | countdown.MINUTES | countdown.SECONDS),
+        );
       } else {
         this.notFound = true;
       }
