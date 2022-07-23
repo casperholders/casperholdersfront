1e144f9eae4d919e953d28c105bfe4797505cdfc
Paul Thebaud <paul.thebaud29@gmail.com>
2022-07-23 18:42:55 +0200

    build: update config, deps and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,15 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'airbnb-base',
     'plugin:vue/recommended',
-    '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'import/order': ['off'], // See https://youtrack.jetbrains.com/issue/WEB-21182
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-      mjs: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-      vue: 'never',
-    }],
+    'import/no-unresolved': ['off'],
+    'import/extensions': ['off'],
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: true,
     }],
